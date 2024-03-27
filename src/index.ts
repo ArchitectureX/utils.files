c40701b0b00f95605a41bd7e0e5f3552f636ccf8
Daniel Cortes <danieldevcode@gmail.com>
2024-03-26 21:29:10 -0700

    fix: add file utils functions

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,181 @@
+import security from '@architecturex/utils.security'
+import slug from '@architecturex/utils.slug'
+
 const files = {
-  bytesToSize: (bytes: any, maxFileSize: number, round?: boolean): any => {
+  bytesToSize: (bytes: number, maxFileSize: number, round?: boolean) => {
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
-    let allowed = true
+    const allowed = bytes <= maxFileSize
 
-    if (bytes > maxFileSize) {
-      allowed = false
+    if (bytes === 0) {
+      return { size: `0 ${sizes[0]}`, allowed }
     }
 
-    const n = Number(bytes)
-
-    // @ts-ignore
-    const i = parseInt(Math.floor(Math.log(n) / Math.log(1024)), 10)
-
-    if (i === 0) {
-      return `${bytes} ${sizes[i]}`
-    }
-
-    let size: any = (bytes / 1024 ** i).toFixed(1)
+    const i = Math.floor(Math.log(bytes) / Math.log(1024))
+    let size = bytes / 1024 ** i
 
     if (round) {
       size = Math.ceil(size)
     }
 
     return {
-      size: `${size} ${sizes[i]}`,
+      size: `${size.toFixed(1)} ${sizes[i]}`,
       allowed
     }
+  },
+  getFileNameAndExtension: (file: any): { fileName: string; extension: string } => {
+    if (!file) {
+      return {
+        fileName: '',
+        extension: ''
+      }
+    }
+
+    const parts = file.split('.')
+    const extension = parts.pop()
+    const fileName = parts.pop()
+
+    return {
+      fileName,
+      extension: extension.toLowerCase()
+    }
+  },
+  getImageData: (file: any): Promise<any> => {
+    return new Promise((resolve, reject) => {
+      const img = new Image()
+
+      img.src = window.URL.createObjectURL(file)
+      img.onload = (): any => resolve({ width: img.width, height: img.height })
+      img.onerror = reject
+    })
+  },
+  getSelectedFile: async (e: any): Promise<any> => {
+    if (e.target.files[0]) {
+      const fileData = e.target.files[0]
+      const fileSize = files.bytesToSize(fileData.size, 52000000)
+      const { fileName, extension } = files.getFileNameAndExtension(fileData.name)
+      const identifier = slug(fileName)
+      const code = security.string.code(4)
+      const isImage = ['png', 'jpg', 'jpeg'].includes(extension)
+
+      let information = ''
+      let url = '/files'
+
+      if (isImage) {
+        const img: any = await files.getImageData(fileData)
+        information = `${img.width}x${img.height}px`
+        url += '/images'
+      }
+
+      return {
+        file: fileData,
+        fileName: `${identifier}_${code}.${extension}`,
+        fileUrl: `${url}/${identifier}_${code}.${extension}`,
+        size: fileSize.size,
+        information
+      }
+    }
+  },
+  findFileByAction: (fileStatus: any, action: 'show' | 'delete' | 'upload') => {
+    return fileStatus.find((image: any) => image.action == action)
+  },
+  getFileNameFromUrl(url: string) {
+    const fileName = url.split('/').pop()
+    return fileName ? fileName : ''
+  },
+  fileStatusActions: async (fileStack: any, deleteCallback: any) => {
+    let fileName
+
+    while (fileStack.length > 1) {
+      const currentFileStatus = fileStack.pop()
+
+      if (currentFileStatus) {
+        fileName = files.getFileNameFromUrl(currentFileStatus.url)
+
+        if (currentFileStatus.action === 'delete' || currentFileStatus.action == 'pending') {
+          // Delete photo from server
+          await deleteCallback(fileName)
+        }
+        if (currentFileStatus.action === 'upload') {
+          // Upload photo to server
+          currentFileStatus.action = 'show'
+          fileStack.unshift(currentFileStatus)
+        }
+      }
+    }
+    return { fileStack: [...fileStack], fileName }
+  },
+  deleteFilesFromServer: async (array: any[], deleteCallback: any) => {
+    for (let i = 0; i < array.length; i++) {
+      const file = array[i]
+      const action = file.action
+      const fileName = files.getFileNameFromUrl(file.url)
+
+      if (action === 'delete' || action === 'upload') {
+        await deleteCallback(fileName)
+      }
+    }
+  },
+  uploadFiles: async (files: any) => {
+    if (!files) {
+      return
+    }
+
+    const fileUploadEndPoint = '/api/v1/uploader'
+    const formData = new FormData()
+
+    files.forEach((file: any) => {
+      formData.append(`files`, file.file)
+    })
+
+    const multiUploadResponse = await fetch(fileUploadEndPoint, {
+      method: 'POST',
+      body: formData
+    })
+
+    console.log('⚡ Files Response', multiUploadResponse)
+    //TODO: handle response
+  },
+  generateUniqueFileName: (originalFileName: string) => {
+    const { fileName, extension } = files.getFileNameAndExtension(originalFileName)
+    const identifier = slug(fileName)
+    const code = security.string.code(4)
+
+    return {
+      fileName: `${identifier}_${code}.${extension}`,
+      nameParts: {
+        identifier,
+        code,
+        extension
+      }
+    }
+  },
+  formatFileData: async (file: File) => {
+    const {
+      nameParts: { identifier, code, extension }
+    } = files.generateUniqueFileName(file.name)
+
+    const fileSize = files.bytesToSize(file.size, 52000000)
+    const isImage = ['png', 'jpg', 'jpeg'].includes(extension)
+    let url = '/files'
+    let information = ''
+
+    if (isImage) {
+      const img = await files.getImageData(file)
+      information = `${img.width}x${img.height}px`
+      url += '/images'
+    }
+
+    return {
+      file,
+      storagePath: `${url}/${identifier}_${code}.${extension}`,
+      size: fileSize.size,
+      information
+    }
+  },
+  formatFileList: async (fileList: FileList) => {
+    const fileListArray = Array.from(fileList)
+    const formattedFileList = fileListArray.map((file) => files.formatFileData(file))
+    return Promise.all(formattedFileList)
   }
 }
 
